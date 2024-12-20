fc8be1467fdd62aff958b53196c843ea68497559
Vladislav <vladislav.volminsky@gmail.com>
2024-12-20 22:33:44 +0200

    Initial commit

--- backend/public/api.php
+++ backend/public/api.php
@@ -1,5 +1,4 @@
 <?php
-echo '123';
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
@@ -7,9 +6,6 @@ header('Access-Control-Allow-Headers: Content-Type, Authorization');
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config/doctrine_config.php';
 require_once __DIR__ . '/../config/purifier_config.php';
-
-
-
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(204);
